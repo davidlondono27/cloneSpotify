46078740dfee171d45dd3c068e4185019c351c8c
David <hlc.david327@gmail.com>
2022-06-25 20:23:09 -0500

    Initial Commit

---
+++ Spotify/SpotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyApp.swift
+//  Spotify
+//
+//  Created by David Londoño on 25/06/22.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
